d8e40d8d1a2c812af5263c31cf3384dd745217e4
Amal Lotfi <esbilgic9@gmail.com>
2026-02-10 20:19:14 +0100

    Add files via upload

---
+++ basic_condition.py
@@ -0,0 +1,16 @@
+# Rain check
+raining = input("Is it raining? (yes/no): \n")
+if raining == "yes":
+    print("take an umbrella")
+else:
+    print("don't take an umbrella")
+
+# Age Verification
+print("Welcome to my application")
+age=int(input("enter your age: \n"))
+if age >= 12:
+    print("Good, you can use the application")
+else:
+    print("Sorry, you can't use the application")
+    # Code block  
+
---
+++ grading_system.py
@@ -0,0 +1,10 @@
+grade = float(input("Enter your score: \n"))
+if grade >= 90:
+    print("Excellent! You did a great job!")
+elif grade >= 75:
+    print("Good job! Well done!")
+elif grade >= 50:
+    print("Accepted! You passed.")
+else:
+    print("Weak! You need to work harder.")
+print("Thank you for using our grading system!")
